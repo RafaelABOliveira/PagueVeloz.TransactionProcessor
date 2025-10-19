db461aa87723e1f692a2b6636217fe3eec6ed0ab
Rafael Oliveira <rafaelntn766@gmail.com>
2025-10-18 22:43:48 -0300

    Retry + backoff policies

--- PagueVeloz.Infrastructure/Persistence/Repositories/TransactionRepository.cs
+++ PagueVeloz.Infrastructure/Persistence/Repositories/TransactionRepository.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Logging;
 using PagueVeloz.Core.Domain.Entities;
 using PagueVeloz.Core.Domain.Enums;
 using PagueVeloz.Core.Domain.Interfaces;
+using PagueVeloz.Infrastructure.Resilience;
+using Polly.Retry;
 
 namespace PagueVeloz.Infrastructure.Persistence.Repositories
 {
@@ -10,63 +12,69 @@ namespace PagueVeloz.Infrastructure.Persistence.Repositories
     {
         private readonly ConnectionFactory _connectionFactory;
         private readonly ILogger _logger;
+        private readonly AsyncRetryPolicy _retryPolicy;
 
         public TransactionRepository(ConnectionFactory connectionFactory, ILogger<TransactionRepository> logger)
         {
             _connectionFactory = connectionFactory;
             _logger = logger;
+            _retryPolicy = PollyPolicies.SqlRetryPolicy(_logger);
         }
 
         public async Task<string> AddAsyncTransactionRegistry(Transaction transaction)
         {
             try
             {
-                _logger.LogInformation("Opening database connection to add new transaction for AccountId {AccountId}", transaction.AccountId);
+                return await _retryPolicy.ExecuteAsync(async () =>
+                {
+                    _logger.LogInformation("Opening database connection to add new transaction for AccountId {AccountId}", transaction.AccountId);
 
-                using var connection = (SqlConnection)_connectionFactory.CreateConnection();
-                await connection.OpenAsync();
-                _logger.LogInformation("Database connection opened. Inserting transaction.");
+                    using var connection = (SqlConnection)_connectionFactory.CreateConnection();
+                    await connection.OpenAsync();
+                    _logger.LogInformation("Database connection opened. Inserting transaction.");
 
-                // Insere a transação sem TransactionId e retorna o Id gerado
-                var insertSql = @"
-                    INSERT INTO [Transaction] (
-                        AccountId, 
-                        TypeId, 
-                        Amount, 
-                        Description,
-                        TransactionId
-                    )
-                    VALUES (
-                        @AccountId, 
-                        @TypeId, 
-                        @Amount, 
-                        @Description,
-                        ''
-                    );
-                    SELECT CAST(SCOPE_IDENTITY() AS int);
-                ";
+                    var insertSql = @"
+                        INSERT INTO [Transaction] (
+                            AccountId, 
+                            TypeId, 
+                            Amount, 
+                            Description,
+                            TransactionId
+                        )
+                        VALUES (
+                            @AccountId, 
+                            @TypeId, 
+                            @Amount, 
+                            @Description,
+                            ''
+                        );
+                        SELECT CAST(SCOPE_IDENTITY() AS int);
+                    ";
 
-                using var insertCommand = new SqlCommand(insertSql, connection);
-                insertCommand.Parameters.AddWithValue("@AccountId", transaction.AccountId);
-                insertCommand.Parameters.AddWithValue("@TypeId", (int)transaction.Type);
-                insertCommand.Parameters.AddWithValue("@Amount", transaction.Amount);
-                insertCommand.Parameters.AddWithValue("@Description", transaction.Description ?? string.Empty);
+                    using var insertCommand = new SqlCommand(insertSql, connection);
+                    insertCommand.Parameters.AddWithValue("@AccountId", transaction.AccountId);
+                    insertCommand.Parameters.AddWithValue("@TypeId", (int)transaction.Type);
+                    insertCommand.Parameters.AddWithValue("@Amount", transaction.Amount);
+                    insertCommand.Parameters.AddWithValue("@Description", transaction.Description ?? string.Empty);
 
-                var insertedId = (int)await insertCommand.ExecuteScalarAsync();
+                    var insertedId = (int?)await insertCommand.ExecuteScalarAsync();
 
-                var selectSql = "SELECT TransactionId FROM [Transaction] WHERE Id = @Id";
-                using var selectCommand = new SqlCommand(selectSql, connection);
-                selectCommand.Parameters.AddWithValue("@Id", insertedId);
-                var transactionId = (string)await selectCommand.ExecuteScalarAsync();
+                    var selectSql = "SELECT TransactionId FROM [Transaction] WHERE Id = @Id";
+                    using var selectCommand = new SqlCommand(selectSql, connection);
+                    selectCommand.Parameters.AddWithValue("@Id", insertedId);
+                    var transactionId = (string?)await selectCommand.ExecuteScalarAsync();
 
-                _logger.LogInformation("Transaction successfully added. AccountId: {AccountId}, Amount: {Amount}, TransactionId: {TransactionId}", transaction.AccountId, transaction.Amount, transactionId);
+                    if (transactionId == null)
+                        throw new InvalidOperationException("TransactionId not found after insert but included in DB by trigger.");
 
-                return transactionId;
+                    _logger.LogInformation("Transaction successfully added. AccountId: {AccountId}, Amount: {Amount}, TransactionId: {TransactionId}", transaction.AccountId, transaction.Amount, transactionId); ;
+                    return transactionId;
+                });
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error adding new transaction for AccountId {AccountId}", transaction.AccountId);
-                throw new InvalidOperationException($"Error adding new transaction: {ex.Message}", ex);
+                throw new InvalidOperationException("Could not add transaction registry", ex);
             }
         }
 
@@ -74,48 +82,51 @@ namespace PagueVeloz.Infrastructure.Persistence.Repositories
         {
             try
             {
-                _logger.LogInformation("Searching transaction by ReferenceId {ReferenceId}", referenceId);
-
-                using var connection = (SqlConnection)_connectionFactory.CreateConnection();
-                await connection.OpenAsync(cancellationToken);
-
-                var sql = @"
-                    SELECT 
-                        Id,
-                        TransactionId,
-                        AccountId,
-                        TypeId,
-                        Amount,
-                        Description,
-                        ReferenceId
-                    FROM [Transaction]
-                    WHERE ReferenceId = @ReferenceId
-                ";
-
-                using var command = new SqlCommand(sql, connection);
-                command.Parameters.AddWithValue("@ReferenceId", referenceId);
-
-                using var reader = await command.ExecuteReaderAsync(cancellationToken);
-                if (await reader.ReadAsync(cancellationToken))
+                return await _retryPolicy.ExecuteAsync(async () =>
                 {
-                    return new Transaction
-                    {
-                        Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                        TransactionId = reader.GetString(reader.GetOrdinal("TransactionId")),
-                        AccountId = reader.GetString(reader.GetOrdinal("AccountId")),
-                        Type = (TransactionType)reader.GetByte(reader.GetOrdinal("TypeId")),
-                        Amount = reader.GetInt64(reader.GetOrdinal("Amount")),
-                        Description = reader.GetString(reader.GetOrdinal("Description")),
-                        ReferenceId = reader.GetString(reader.GetOrdinal("ReferenceId"))
-                    };
-                }
+                    _logger.LogInformation("Searching transaction by ReferenceId {ReferenceId}", referenceId);
 
-                return null;
+                    using var connection = (SqlConnection)_connectionFactory.CreateConnection();
+                    await connection.OpenAsync(cancellationToken);
+
+                    var sql = @"
+                        SELECT 
+                            Id,
+                            TransactionId,
+                            AccountId,
+                            TypeId,
+                            Amount,
+                            Description,
+                            ReferenceId
+                        FROM [Transaction]
+                        WHERE ReferenceId = @ReferenceId
+                    ";
+
+                    using var command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@ReferenceId", referenceId);
+
+                    using var reader = await command.ExecuteReaderAsync(cancellationToken);
+                    if (await reader.ReadAsync(cancellationToken))
+                    {
+                        return new Transaction
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            TransactionId = reader.GetString(reader.GetOrdinal("TransactionId")),
+                            AccountId = reader.GetString(reader.GetOrdinal("AccountId")),
+                            Type = (TransactionType)reader.GetByte(reader.GetOrdinal("TypeId")),
+                            Amount = reader.GetInt64(reader.GetOrdinal("Amount")),
+                            Description = reader.GetString(reader.GetOrdinal("Description")),
+                            ReferenceId = reader.GetString(reader.GetOrdinal("ReferenceId"))
+                        };
+                    }
+
+                    return null;
+                });
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error searching transaction by ReferenceId {ReferenceId}", referenceId);
-                throw new InvalidOperationException($"Error searching transaction: {ex.Message}", ex);
+                throw new InvalidOperationException("Could not find Transaction by ReferenceId", ex);
             }
         }
     }
--- PagueVeloz.Infrastructure/Resilience/RetryPolicy.cs
+++ PagueVeloz.Infrastructure/Resilience/RetryPolicy.cs
@@ -1,6 +1,24 @@
-﻿namespace PagueVeloz.Infrastructure.Resilience
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
+
+namespace PagueVeloz.Infrastructure.Resilience
 {
-    internal class RetryPolicy
+    public static class PollyPolicies
     {
+        public static AsyncRetryPolicy SqlRetryPolicy(ILogger logger)
+        {
+            return Policy
+                .Handle<SqlException>()
+                .Or<InvalidOperationException>()
+                .WaitAndRetryAsync(
+                    retryCount: 3,
+                    sleepDurationProvider: attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt - 1)),
+                    onRetry: (exception, timeSpan, retryCount, context) =>
+                    {
+                        logger.LogWarning(exception, "Retry {RetryCount} after {Delay}s due to error: {Message}", retryCount, timeSpan.TotalSeconds, exception.Message);
+                    });
+        }
     }
 }
